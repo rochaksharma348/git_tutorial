894e33621d2e69b64ff22937a8c3e5d4715f9a0d
rochaksharma348 <64397826+rochaksharma348@users.noreply.github.com>
2023-01-05 12:15:27 +0530

    Hungry class updated to make it functional

--- src/Hungry.java
+++ src/Hungry.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Hungry {
     public static void main(String[] args) {
-        System.out.println("I am hungry");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Are you hungry?");
+        String ans = sc.nextLine();
+
+        if (ans.equals("yes")) {
+            System.out.println("Eat a samosa");
+        } else {
+            System.out.println("go, do your homework");
+        }
+
     }
 }
